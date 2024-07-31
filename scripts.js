3b8bc0389fce53a7f54680be483fbdc6f84750e3
Tarun Yadav <ytarun303@gmail.com>
2024-07-31 10:57:10 +0530

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href = 'project1/index.html';
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = 'project2/index.html';
+});
+document.getElementById('button3').addEventListener('click', function() {
+    window.location.href = 'project3/index.html';
+});
